3688bdc7f1818df3c644a1f53beaf862db2b7d92
Jesse <jesse.lare@gmail.com>
2024-11-08 23:48:15 -0500

    updating e-commerce project photo

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -13,7 +13,7 @@ const projects = [
   {
     title: 'E-Commerce Backend',
     description: 'Using Object Relational Mapping (ORM) to create a backend for an e-commerce site.',
-    image: 'https://images.unsplash.com/photo-1661956602116-aa6865609028?auto=format&fit=crop&q=80&w=800&h=600',
+    image: 'https://plus.unsplash.com/premium_photo-1678566111481-8e275550b700?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     tech: ['Node.js', 'Express', 'MySQL', 'Insomnia', 'Sequelize'],
     githubUrl: 'https://github.com/SideControlJS/e-commerce'
   },
